60c78388d6889eed621cebb81881ee9b0af18f5d
Dark Angel <jonathanyombo@gmail.com>
2019-02-05 21:55:00 +0200

    add Personnel method and attributes

--- src/org/pbreakers/Personnel.java
+++ src/org/pbreakers/Personnel.java
@@ -1,4 +1,46 @@
 package org.pbreakers;
 
-public abstract class Personnel implements PersonnelInterface{
+abstract class Personnel implements PersonnelInterface{
+
+    private String nom, postnom, prenom, matricule;
+    private static int nb = 0;
+
+    Personnel(String nom, String postnom, String prenom){
+        this.setNom(nom);
+        this.setPostnom(postnom);
+        this.setPrenom(prenom);
+        this.setMatricule("15"+(nom.substring(0,1)+postnom.substring(0,1)+prenom.substring(0,1)).toUpperCase());
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPostnom() {
+        return postnom;
+    }
+
+    public void setPostnom(String postnom) {
+        this.postnom = postnom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
 }
