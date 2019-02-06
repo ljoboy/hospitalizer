aabf37658f8276db97b87fa2829c7244fd146a5f
Dark Angel <jonathanyombo@gmail.com>
2019-02-06 15:49:52 +0200

    add getter for nb && update matricule generation

--- src/org/pbreakers/Personnel.java
+++ src/org/pbreakers/Personnel.java
@@ -3,13 +3,25 @@ package org.pbreakers;
 abstract class Personnel implements PersonnelInterface{
 
     private String nom, postnom, prenom, matricule;
-    private static int nb = 0;
+    static private int nb = 0;
 
     Personnel(String nom, String postnom, String prenom){
         this.setNom(nom);
         this.setPostnom(postnom);
         this.setPrenom(prenom);
-        this.setMatricule("15"+(nom.substring(0,1)+postnom.substring(0,1)+prenom.substring(0,1)).toUpperCase());
+        nb++;
+        this.setMatricule("19"+(nom.substring(0,1)+postnom.substring(0,1)+prenom.substring(0,1)).toUpperCase() + this.getNb());
+    }
+
+    private String getNb(){
+        int count = 0;
+        StringBuilder nb2 = new StringBuilder();
+        while(nb > 0) {
+            nb = nb / 10;
+            count = count + 1;
+        }
+        for (int i = count; i <  4; i++) nb2.append(0);
+        return nb2 + Integer.toString(nb);
     }
 
     public String getNom() {
