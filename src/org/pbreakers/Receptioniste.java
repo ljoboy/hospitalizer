9023fa68d3cb4578fe734f8de360664909a9d6e0
Dark Angel <jonathanyombo@gmail.com>
2019-02-06 15:53:10 +0200

    Receptioniste Constructor

--- src/org/pbreakers/Receptioniste.java
+++ src/org/pbreakers/Receptioniste.java
@@ -1,4 +1,7 @@
 package org.pbreakers;
 
 public class Receptioniste extends Personnel {
+    Receptioniste(String nom, String postnom, String prenom) {
+        super(nom, postnom, prenom);
+    }
 }
