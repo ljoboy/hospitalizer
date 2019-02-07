a660d54160aad0d81a6e66cc91ff802aca0fe020
Dark Angel <jonathanyombo@gmail.com>
2019-02-07 08:29:46 +0200

    add attributes and methods for Consultation

--- src/org/pbreakers/Consultation.java
+++ src/org/pbreakers/Consultation.java
@@ -1,4 +1,21 @@
 package org.pbreakers;
 
+import java.util.Date;
+
 class Consultation {
+    private Date date = new Date();
+    private Docteur docteur;
+    private Date rdv;
+
+    Consultation(Docteur doc){
+        this.docteur = doc;
+    }
+
+    public Date getRdv() {
+        return rdv;
+    }
+
+    public void setRdv(Date rdv) {
+        this.rdv = rdv;
+    }
 }
