ad5beca3481a1c61cadd8d5dd5dc5ce6134233ef
Dark Angel <jonathanyombo@gmail.com>
2019-02-07 08:31:20 +0200

    add Consultation && Ordonnance

--- src/org/pbreakers/Docteur.java
+++ src/org/pbreakers/Docteur.java
@@ -4,4 +4,12 @@ public class Docteur extends Personnel {
     Docteur(String nom, String postnom, String prenom) {
         super(nom, postnom, prenom);
     }
+
+    Consultation consuler(){
+        return new Consultation(this);
+    }
+
+    Ordonnance ordonnance(Fiche f){
+        return new Ordonnance(f, this);
+    }
 }
