b9b15fff9a9d617fd7c44702ab1603a4d2a895dd
Dark Angel <jonathanyombo@gmail.com>
2019-02-11 20:37:41 +0200

    add new Dosage

---
+++ src/org/pbreakers/Dosage.java
@@ -0,0 +1,28 @@
+package org.pbreakers;
+
+class Dosage {
+    private int nbJour;
+    private int nbComprime;
+    private int agePatient;
+    private int nbFois;
+
+    Dosage(){
+
+    }
+
+    public int getAgePatient() {
+        return agePatient;
+    }
+
+    public int getNbComprime() {
+        return nbComprime;
+    }
+
+    public int getNbFois() {
+        return nbFois;
+    }
+
+    public int getNbJour() {
+        return nbJour;
+    }
+}
