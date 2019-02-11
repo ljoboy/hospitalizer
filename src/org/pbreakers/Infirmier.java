345b8eb90377c2fb45be28d11d7ceddc1c3c5e55
Dark Angel <jonathanyombo@gmail.com>
2019-02-12 01:45:16 +0200

    add method to add patient to chamber

--- src/org/pbreakers/Infirmier.java
+++ src/org/pbreakers/Infirmier.java
@@ -1,7 +1,16 @@
 package org.pbreakers;
 
-public class Infirmier extends Personnel {
+class Infirmier extends Personnel {
     Infirmier(String nom, String postnom, String prenom) {
         super(nom, postnom, prenom);
+    }
+
+    void patientChambre(Chambre ch, Patient p){
+        if (ch.patients.size() < ch.getNbLit()) {
+            ch.patients.add(p);
+            System.out.println("Patient ajouter avec succes");
+        }else{
+            System.out.println("La chambre est pleine");
+        }
     }
 }
