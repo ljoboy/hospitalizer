1744b34cd7acb8ebbae7fd9e4215fbcee7485a04
Dark Angel <jonathanyombo@gmail.com>
2019-02-12 01:43:48 +0200

    implement affichable

--- src/org/pbreakers/Consultation.java
+++ src/org/pbreakers/Consultation.java
@@ -2,7 +2,7 @@ package org.pbreakers;
 
 import java.util.Date;
 
-class Consultation {
+class Consultation implements Affichable{
     private Date date = new Date();
     private Docteur docteur;
     private Date rdv;
@@ -17,5 +17,9 @@ class Consultation {
 
     public void setRdv(Date rdv) {
         this.rdv = rdv;
+    }
+
+    public void afficher(){
+        System.out.println("Consultation du "+date.toString()+" par le Dr "+docteur.getNom()+" "+docteur.getPostnom()+" "+docteur.getPrenom());
     }
 }
