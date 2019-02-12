4e1bda1b2e0df9b592c204bc6cd27bd954649afa
Dark Angel <jonathanyombo@gmail.com>
2019-02-12 08:49:45 +0200

    extends Personne

--- src/org/pbreakers/Personnel.java
+++ src/org/pbreakers/Personnel.java
@@ -1,15 +1,11 @@
 package org.pbreakers;
 
-abstract class Personnel implements PersonnelInterface{
-
-    private String nom, postnom, prenom, matricule;
+abstract class Personnel extends Personne implements PersonnelInterface{
     static private int nb = 0;
-
-    Personnel(String nom, String postnom, String prenom){
-        this.setNom(nom);
-        this.setPostnom(postnom);
-        this.setPrenom(prenom);
-        nb++;
+    private String matricule;
+    Personnel(String nom, String postnom, String prenom) {
+        super(nom, postnom, prenom);
+        Personnel.nb++;
         this.setMatricule("19"+(nom.substring(0,1)+postnom.substring(0,1)+prenom.substring(0,1)).toUpperCase() + this.getNb());
     }
 
@@ -24,40 +20,11 @@ abstract class Personnel implements PersonnelInterface{
         return nb2 + Integer.toString(nb);
     }
 
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPostnom() {
-        return postnom;
-    }
-
-    public void setPostnom(String postnom) {
-        this.postnom = postnom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
     public String getMatricule() {
         return matricule;
     }
 
     private void setMatricule(String matricule) {
         this.matricule = matricule;
-    }
-
-    @Override
-    public void afficher() {
-        System.out.println(this.getNom()+" "+this.getPostnom()+" "+this.getPrenom());
     }
 }
